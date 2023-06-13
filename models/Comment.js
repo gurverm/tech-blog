622188c6c25c6c930644e762a6e105358ddf1ce1
Gurveer Madurai <maduraigurveer@gmail.com>
2023-06-12 21:15:21 -0700

    comment logic added to db

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "posts",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+model.exports = Comment;
