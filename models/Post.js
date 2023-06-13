efbf9814ee4c55502be05b38a4d0638f823cf6a4
Gurveer Madurai <maduraigurveer@gmail.com>
2023-06-12 21:17:55 -0700

    post logic added to db

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    post_body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+model.exports = Post;
